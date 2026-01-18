b5338131178c7b9ae2b09e73e83317ce068a038a
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2026-01-18 21:03:14 +0800

    backup files

---
+++ test/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/routes/**/*.{svelte,ts,js,html}',
+    './src/lib/**/*.{svelte,ts,js,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
